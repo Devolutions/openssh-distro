d9719cb6f896cafc44a0034a35f96a216e709652
Marc-André Moreau <mamoreau@devolutions.net>
2024-02-06 13:14:00 -0500

    add Win32LibFIDO2Patch.ps1 script

--- scripts/Win32LibFIDO2Patch.ps1
+++ scripts/Win32LibFIDO2Patch.ps1
@@ -24,77 +24,77 @@ $MajorVersion = $VersionParts.Major
 $MinorVersion = $VersionParts.Minor
 $PatchVersion = $VersionParts.Build
 
-$UpstreamGitRepo = "https://github.com/libressl/portable"
+$UpstreamGitRepo = "https://github.com/Yubico/libfido2"
 if ([string]::IsNullOrEmpty($UpstreamGitTag)) {
-    $UpstreamGitTag = "v${Version}"
+    $UpstreamGitTag = "${Version}"
 }
 
-$DownstreamGitRepo = "https://github.com/PowerShell/LibreSSL"
+$DownstreamGitRepo = "https://github.com/PowerShell/libfido2"
 if ([string]::IsNullOrEmpty($DownstreamGitTag)) {
-    $DownstreamGitTag = "V${Version}.0"
+    $DownstreamGitTag = "${Version}"
 }
 
 if ([string]::IsNullOrEmpty($TemporaryPath)) {
-    $TemporaryPath = Join-Path ([System.IO.Path]::GetTempPath()) "libressl-git"
+    $TemporaryPath = Join-Path ([System.IO.Path]::GetTempPath()) "libfido2-git"
 }
 
 if ([string]::IsNullOrEmpty($OutputPath)) {
-    $OutputPath = Join-Path $TemporaryPath "libressl-patches"
+    $OutputPath = Join-Path $TemporaryPath "libfido2-patches"
 }
 
 # create temporary directory for upstream and downstream git repositories
 New-Item -Path $TemporaryPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 Push-Location && Set-Location $TemporaryPath
 
-# clone upstream libressl git repository, remove .git directory:
-Remove-Item -Path libressl-upstream -Recurse -Force -ErrorAction SilentlyContinue
-git clone -b $UpstreamGitTag --depth 1 $UpstreamGitRepo libressl-upstream
+# clone upstream libfido2 git repository, remove .git directory:
+Remove-Item -Path libfido2-upstream -Recurse -Force -ErrorAction SilentlyContinue
+git clone -b $UpstreamGitTag --depth 1 $UpstreamGitRepo libfido2-upstream
 
-# clone downstream libressl git repository, remove .git directory:
-Remove-Item -Path libressl-downstream -Recurse -Force -ErrorAction SilentlyContinue
-git clone -b $DownstreamGitTag --depth 1 $DownstreamGitRepo libressl-downstream
+# clone downstream libfido2 git repository, remove .git directory:
+Remove-Item -Path libfido2-downstream -Recurse -Force -ErrorAction SilentlyContinue
+git clone -b $DownstreamGitTag --depth 1 $DownstreamGitRepo libfido2-downstream
 
 # generate single patch file from upstream and downstream directories:
-diff -ruN --exclude=.git libressl-upstream/ libressl-downstream/ > Win32-LibreSSL.patch
+diff -ruN --exclude=.git libfido2-upstream/ libfido2-downstream/ > Win32-libfido2.patch
 
 # generate a list of binary files which differ between upstream and downstream directories:
-diff -ruN --exclude=.git libressl-upstream/ libressl-downstream/ | grep "^Binary files" |
-    awk -F ' and ' '{split($1, a, " "); sub("^[^/]*/", "", a[3]); print a[3]}' > Win32-LibreSSL-bin.txt
+diff -ruN --exclude=.git libfido2-upstream/ libfido2-downstream/ | grep "^Binary files" |
+    awk -F ' and ' '{split($1, a, " "); sub("^[^/]*/", "", a[3]); print a[3]}' > Win32-libfido2-bin.txt
 
 # copy binary (non-textual) files from downstream to upstream directory
-foreach($BinaryFile in (Get-Content ./Win32-LibreSSL-bin.txt)) {
+foreach($BinaryFile in (Get-Content ./Win32-libfido2-bin.txt)) {
     Write-Host "Copying binary file $BinaryFile"
-    $DestinationFile = Join-Path (Get-Location) "libressl-upstream" $BinaryFile
+    $DestinationFile = Join-Path (Get-Location) "libfido2-upstream" $BinaryFile
     $DestinationPath = [System.IO.Path]::GetDirectoryName($DestinationFile)
     New-Item -Path $DestinationPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-    Copy-Item "./libressl-downstream/$BinaryFile" $DestinationFile -Force
+    Copy-Item "./libfido2-downstream/$BinaryFile" $DestinationFile -Force
 }
-Remove-Item ./Win32-LibreSSL-bin.txt | Out-Null
+#Remove-Item ./Win32-libfido2-bin.txt | Out-Null
 
-# apply downstream Win32-LibreSSL patches on top of upstream LibreSSL repository
-cd libressl-upstream
+# apply downstream Win32-libfido2 patches on top of upstream libfido2 repository
+cd libfido2-upstream
 $Win32PatchBranch = "Win32-v${Version}"
 git branch $Win32PatchBranch && git checkout $Win32PatchBranch
-Get-Content ../Win32-LibreSSL.patch -Raw | patch -s -p1
-Remove-Item ../Win32-LibreSSL.patch | Out-Null
+Get-Content ../Win32-libfido2.patch -Raw | patch -s -p1
+Remove-Item ../Win32-libfido2.patch | Out-Null
 
 # Create new commits from the applied changes
 git add $(git ls-files -o --exclude-standard)
-git commit -m "Win32-LibreSSL v${Version} added files"
+git commit -m "Win32-libfido2 v${Version} added files"
 git add -A
-git commit -m "Win32-LibreSSL v${Version} modified files"
+git commit -m "Win32-libfido2 v${Version} modified files"
 git format-patch -2
 cd ..
 
 New-Item -Path $OutputPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 Get-Item "$OutputPath/*v${Version}*.patch" | Remove-Item
-Move-Item ./libressl-upstream/*.patch $OutputPath
+Move-Item ./libfido2-upstream/*.patch $OutputPath
 
-Write-Host "Created Win32-LibreSSL v$Version patches on top of $UpstreamGitTag upstream git tag"
-Write-Host "From your libressl local git repository, run the following to import them:"
+Write-Host "Created Win32-libfido2 v$Version patches on top of $UpstreamGitTag upstream git tag"
+Write-Host "From your libfido2 local git repository, run the following to import them:"
 Write-Host "git checkout $UpstreamGitTag"
 Write-Host "git checkout -b v${Version}-patches"
 Write-Host "git am --whitespace=nowarn $OutputPath/*v${Version}*.patch"
 
 # Sample usage:
-# ./Win32LibreSSLPatch.ps1 3.8.2
+# ./Win32LibFIDO2Patch.ps1 1.14.0
